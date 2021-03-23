315365ada12cfd0affa560720a3c10d65f2f3a06
danburudoa <yumamukai515@gmail.com>
2021-03-23 11:50:33 +0900

    修正３回目

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', () => {
-    // 
+    
     const priceInput = document.getElementById("item-price");    
     priceInput.addEventListener("input", () => {
-       const inputValue = priceInput.value; //priceInput.valueのvalue部分は、IDで取得してきたHTMlの中から、値の部分だけを選択していることが
+       const inputValue = priceInput.value; //priceInput.valueのvalue部分は、IDで取得してきたHTMlの中から、値の部分だけを選択している。
         
-    //
+    
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1)); //Math.floorは、計算するときに付けなければいけない。innerHTMLはそれを埋め込んでいる。
    
-    //
+    
     const profitNumber = document.getElementById("profit")
     const value_result = inputValue * 0.1
     profitNumber.innerHTML = (Math.floor(inputValue - value_result));
