897555c8060f555180ce05d730bd00b139bc5e35
danburudoa <yumamukai515@gmail.com>
2021-03-23 23:26:33 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!,only:[:new,:create]
+    before_action :authenticate_user!,only:[:new,:create,:edit,:update]
+    before_action :move_to_index, only: [:edit]
 
     def index
       @products = Product.includes(:user).order("created_at DESC")
@@ -22,9 +23,33 @@ class ItemsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
+    def edit
+      @product = Product.find(params[:id])
+      unless @product.user_id == current_user.id
+        redirect_to action: :index
+      end
+    end
+
+    def update
+      @product = Product.find(params[:id])
+      @product.update(product_params)
+      if @product.save
+        redirect_to item_path(@product.id)
+      else
+        render :edit
+      end
+    end
+
     private
 
     def product_params
         params.require(:product).permit(:name,:describe,:category_id,:condition_id,:charge_id,:delivery_id,:area_id,:price,:image).merge(user_id: current_user.id)
     end
+
+    def move_to_index
+      unless user_signed_in? 
+        redirect_to action: :index
+      end
+    end
 end
+
