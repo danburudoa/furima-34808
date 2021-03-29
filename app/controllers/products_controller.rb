9b763d6a85b74c99dfadd997085256902bec888e
danburudoa <yumamukai515@gmail.com>
2021-03-29 16:16:34 +0900

    修正4回目

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
     before_action :authenticate_user!,only:[:new,:create,:edit,:update]
     before_action :set_tweet, only: [:edit, :show,:update,:destroy]
-    before_action :move_to_index, only: [:edit,:edit,:update,:destroy]
+    before_action :move_to_index, only: [:edit,:update,:destroy]
     
     def index
       @products = Product.includes(:user).order("created_at DESC")
